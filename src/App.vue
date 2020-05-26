11a2f3e735038f6e93b8a9bb565538ca857134e1
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-05-26 13:51:25 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <draggable element="ul">
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </draggable>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import draggable from "vuedraggable"
 
-#nav {
-  padding: 30px;
+export default {
+  name: "App",
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    draggable
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    tasks: [
+        "task1",
+        "task2",
+        "task3",
+        "task4",
+      ]
+  }),
+};
+</script>
